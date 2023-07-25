9a30ff5a1cd708e25d1b0a64070e999074e3e4e2
Vica Fresinsya <vicafresinsya@gmail.com>
2023-07-25 20:51:59 +0700

    contact

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -6,17 +6,17 @@ import Image from 'next/image'
 const page = () => {
   return (
     <body>
-      <h1>Contact Us</h1>
+      <h1 className='jcontact'>Contact Us</h1>
       
       <div className='contact '>
         
         <button className='btn'><a className='btn2 fa' href="https://github.com/Fresinsya" target="_blank">Github</a></button>
-        <button className='btn'><p className='btn2 ig'>Instagram</p></button>
-        <button className='btn'><p className='btn2 wa'>Whatsapp</p></button>
+        <button className='btn'><a className='btn2 ig' href="https://www.instagram.com/vfresinsya/" target="_blank">Instagram</a></button>
+        <button className='btn'><a className='btn2 wa' href="http://wa.me/62895353720203" target="_blank">Whatsapp</a></button>
 
       <div className='email'>
-        <Image id='amplop' src="/asset/amplop.png" width={350} height={350} alt="coba" />
-        <Image id='kertas' src="/asset/card.jpg" width={550} height={330} alt="coba" />
+        <Image className='amplop' src="/asset/amplop.png" width={450} height={450} alt="coba" />
+        <Image className='kertas' src="/asset/card.jpg" width={850} height={530} alt="coba" />
       </div>
 
       </div>
