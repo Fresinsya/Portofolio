1a238ca49a61014edc88756d13e708d376fe95d4
Vica Fresinsya <vicafresinsya@gmail.com>
2023-07-25 12:42:37 +0700

    contact

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -4,15 +4,25 @@ import Image from 'next/image'
 
 
 const page = () => {
-    return (
-        <div className='contact' style={{}}>
-          <Image id='amplop' src="/asset/amplop.png" width={350} height={350} alt="coba" />
-          <Image id='kertas' src="/asset/card.jpg" width={550} height={330}  alt="coba" />
-          
-          <button class='btn'>facebook</button>
-          
-        </div>
-    )
+  return (
+    <body>
+      <h1>Contact Us</h1>
+      
+      <div className='contact '>
+        
+        <button className='btn'><a className='btn2 fa' href="https://github.com/Fresinsya" target="_blank">Github</a></button>
+        <button className='btn'><p className='btn2 ig'>Instagram</p></button>
+        <button className='btn'><p className='btn2 wa'>Whatsapp</p></button>
+
+      <div className='email'>
+        <Image id='amplop' src="/asset/amplop.png" width={350} height={350} alt="coba" />
+        <Image id='kertas' src="/asset/card.jpg" width={550} height={330} alt="coba" />
+      </div>
+
+      </div>
+
+    </body>
+  )
 }
 
 export default page
